edc5edc78cdfc0ad6780b2bfb4097d0187c5ec32
Jedjoud10 <jribijed6@gmail.com>
2019-11-18 21:45:49 -0500

    update

---
+++ Assets/Play Mode/Interpreter/SaverLoader.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SaverLoader
+{
+    public void Save(string fileName) 
+    {
+    
+    }
+    public void Load(string fileName) 
+    {
+    
+    }
+}
